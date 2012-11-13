85cf2c9b1ae45075f6e131b00ee78bb39ace3d42
julio <julio336@hotmail.com>
2012-11-12 20:52:28 -0600

    Facebook login

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '439343236107925', 'fb1c82f1f5893548e750e18e0b67362e',
+  provider :facebook, '163655183779733', '7db1f691e6ca3a7f9ebbf0366662be8e',
   		 	:scope => 'email,user_birthday,read_stream, publish_stream'
   #provider :twitter, "bh0NqOQkC1Hi1M8ib37jQ", "yEZGPrpWeXzRdYeqLuxsNs2IwkW961Lc6f32HQFKU"
 end
