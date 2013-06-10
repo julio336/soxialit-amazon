1c65937ad0d095d98530eea736570c9ba07812bc
julio <julio336@hotmail.com>
2013-06-10 09:35:04 -0500

    carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,6 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
-    :provider               => 'AWS',       # required
-    :aws_access_key_id      => 'AKIAISOYROBHPU657TUA',       # required
-    :aws_secret_access_key  => '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'       # required
+         # required
   }
   config.fog_directory  = 'soxialit_form_paintings'                     # required
   
